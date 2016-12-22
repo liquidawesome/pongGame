a98ad2f43d2a5f08019b31f4a457385a36c502c6
Sean McCartan <sean2223@gmail.com>
2016-12-22 17:15:01 -0500

    Added black background to panel
    rearranged code in main method

--- src/ponggame/Ping.java
+++ src/ponggame/Ping.java
@@ -10,9 +10,12 @@ public class Ping extends JPanel implements ActionListener{
     /*Variables*/
     Timer t = new Timer(5, this);
     
+    double width = this.getWidth(), height = this.getHeight();
+    
     /*Constructors*/
     public Ping(){
-        
+        setFocusable(true);
+        repaint();
     }
     
     /*Methods*/
@@ -23,6 +26,10 @@ public class Ping extends JPanel implements ActionListener{
     public void paint(Graphics g){
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
+        
+        g2.setPaint(Color.BLACK);
+        g2.fill(new Rectangle2D.Double(0,0,500,500 )); //TODO make square change based on width/height
+        
     }//end paint
     
 }//end Ping
--- src/ponggame/UsePong.java
+++ src/ponggame/UsePong.java
@@ -6,15 +6,17 @@ import javax.swing.JFrame;
 
 public class UsePong {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Pong");
         Ping p = new Ping();
-        
+        JFrame frame = new JFrame("Pong");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(p);
+        frame.pack();        
         frame.setSize(500,500);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         frame.setLocation(550,200);
         
-        frame.add(p);
+        
         
         
     }//end main
